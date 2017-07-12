d40a7338560340d13bab70ebd950638dcae6b482
Ilya Semenov <semenov@inetss.com>
2017-07-12 17:03:48 +0700

    pypi 0.0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='aiohttp_session_flash',
-	version='0.0.3',
+	version='0.0.4',
 	url='https://github.com/IlyaSemenov/aiohttp_session_flash',
 	license='BSD',
 	author='Ilya Semenov',
